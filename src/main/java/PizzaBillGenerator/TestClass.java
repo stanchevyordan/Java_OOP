e394390efc1354bfa6049e31e634f74e1e6be571
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2023-12-21 23:19:31 +0200

    Create TestClass.java

---
+++ src/main/java/PizzaBillGenerator/TestClass.java
@@ -0,0 +1,14 @@
+package PizzaBillGenerator;
+
+import java.util.Scanner;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i <= num; i++) {
+            System.out.println("Plamen e pedal");
+        }
+    }
+}
